539514088606550bda75401ff84227d399be2cd9
NilSagor <masum1201@gmail.com>
2019-01-06 22:46:23 +0800

    listview and detail view modified

--- tweets/tweet/views.py
+++ tweets/tweet/views.py
@@ -12,6 +12,14 @@ def tweet_detail_view(request, pk):
 def tweet_list_view(request):
 	return render(request, 'tweet/list_view.html', {})
 """
+class TweetListView(generic.ListView):
+	model = Tweet
+	template_name = 'tweet/list_view.html'	
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(TweetListView, self).get_context_data(*args, **kwargs)
+		#context['-another_list'] = Tweet.objects.all()
+		return context
 
 class TweetDetailView(generic.DetailView):
 	model = Tweet
