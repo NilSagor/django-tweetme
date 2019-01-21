78586606eb9bb352f9fe06e6d88785c14c2b6908
NilSagor <masum1201@gmail.com>
2019-01-21 21:37:10 +0800

    create tweet list view

--- tweets/tweet/forms.py
+++ tweets/tweet/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import Tweet
 
 class TweetModelForm(forms.ModelForm):
+	content = forms.CharField(
+		label = '',
+		widget=forms.Textarea(
+			attrs={"placeholder":"Your message",
+					"class":"form-context" }
+				))
 	class Meta:
 		model = Tweet
 		fields = [
