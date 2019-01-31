d477ec37b27fe073de84f0ebae24b7c2842869f1
NilSagor <masum1201@gmail.com>
2019-01-31 16:47:21 +0800

    user display serializer

--- tweets/tweet/api/serializers.py
+++ tweets/tweet/api/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from tweet.models import Tweet 
 
+from accounts.api.serializers import UserDisplaySerializer
 
 
 
 class TweetModelSerializer(serializers.ModelSerializer):
+	user = UserDisplaySerializer()
 	class Meta:
 		model = Tweet
 		fields = [
